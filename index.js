6972ca37c33cda732a4b90c5ece9e48b0eaa73dc
Voyager56 <78099948+Voyager56@users.noreply.github.com>
2022-02-05 02:20:17 +0400

    up

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// import three js 
-import * as THREE from 'three';
+const input = document.querySelector("input");
+const priceTag = document.querySelector(".price");
+function setBackgroundSize(input) {
+  input.style.setProperty("--background-size", `${getBackgroundSize(input)}%`);
+}
+
+setBackgroundSize(input);
+
+input.addEventListener("input", () => {
+    setBackgroundSize(input);
+    const price = input.value * 16;
+    priceTag.innerHTML = `${price}$`;
+});
+
+function getBackgroundSize(input) {
+  const min = +input.min || 0;
+  const max = +input.max || 100;
+  const value = +input.value;
+
+  const size = (value - min) / (max - min) * 100;
+
+  return size;
+}
